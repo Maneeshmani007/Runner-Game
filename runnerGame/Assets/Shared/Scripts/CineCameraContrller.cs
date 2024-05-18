667c2f26fb1348cb19bfbbbfeb5ff141dfb8b01b
FirozSumra <firoz@upsurge.in>
2024-05-18 11:32:07 +0530

    Added skyboxes

---
+++ runnerGame/Assets/Shared/Scripts/CineCameraContrller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using HyperCasual.Core;
+
+public class CineCameraContrller : AbstractSingleton<CineCameraContrller>
+{
+    public CinemachineVirtualCamera CMvcam1;
+    //CinemachineVirtualCamera cinemachineVirtualCamera;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
